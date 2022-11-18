9b0e385dfc03a173eeaeda3b811de8690ab09a29
Angel <a20190192@pucp.edu.pe>
2022-11-18 16:46:35 -0500

    Nuevo layout

---
+++ app/src/main/java/com/example/labiot5/Entity/Actividad.java
@@ -0,0 +1,9 @@
+package com.example.labiot5.Entity;
+
+public class Actividad {
+    private String titulo;
+    private String descripcion;
+    private String imagenUrl;
+    private long datetimeInicio;
+    private long datetimeFin;
+}
