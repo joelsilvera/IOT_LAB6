4616fbb724d8842d5a903ab7532c5b12df8202a4
Angel <a20190192@pucp.edu.pe>
2022-11-19 22:56:10 -0500

    Entity actividad

--- app/src/main/java/com/example/labiot5/Entity/Actividad.java
+++ app/src/main/java/com/example/labiot5/Entity/Actividad.java
@@ -5,6 +5,66 @@ public class Actividad {
     private String descripcion;
     private String imagenUrl;
     private String fecha;
-    private long datetimeInicio;
-    private long datetimeFin;
+    private String horaInicio;
+    private String horaFin;
+
+    public Actividad() {
+    }
+
+    public Actividad(String titulo, String descripcion, String imagenUrl, String fecha, String horaInicio, String horaFin) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imagenUrl = imagenUrl;
+        this.fecha = fecha;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
+    }
 }
